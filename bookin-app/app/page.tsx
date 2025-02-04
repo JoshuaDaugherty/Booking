a43d66096684878ac209cee03ca198e9577094d4
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2025-02-04 09:18:35 -0600

    First commit

--- bookin-app/app/page.tsx
+++ bookin-app/app/page.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 export default function Home() {
   return (
     <section className="text-3xl flex min-h-screen justify-center items-center">
-      <h2 className="font-semibold">Booking App</h2>
+      <h2 className="font-semibold">Booking App Project</h2>
     </section>
   );
 }
