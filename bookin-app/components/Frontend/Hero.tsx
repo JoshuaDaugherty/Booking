80b5e2d5989a022814b713562c931eb129e8fa05
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2025-02-05 11:41:07 -0600

    Update hero.tsx

--- bookin-app/components/Frontend/Hero.tsx
+++ bookin-app/components/Frontend/Hero.tsx
@@ -124,15 +124,7 @@ const Hero = () => {
 
 export default Hero;
 
-const SingleImage = ({ href, imgSrc }:{href:string,imgSrc:string}) => {
-  return (
-    <>
-      <a href={href} className="flex w-full items-center justify-center">
-        <img src={imgSrc} alt="brand image" className="h-10 w-full" />
-      </a>
-    </>
-  );
-};
+
 
 
 
