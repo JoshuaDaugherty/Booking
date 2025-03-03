32e12acd7d48cc1f7af757b21ccdcf674b3de156
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2025-03-03 11:35:06 -0600

    update

--- bookin-app/app/(front)/verify-account/[id]/page.tsx
+++ bookin-app/app/(front)/verify-account/[id]/page.tsx
@@ -3,7 +3,11 @@
 
 import { getUserById } from "@/actions/users";
 import VerifyTokenForm from "@/components/Frontend/VerifyTokenForm";
-export default async function VerifyAccount({params:{id}}:{params:{id:string}}) {
+export default async function VerifyAccount({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
   //Get a User from db
   const user = await getUserById(id);
   const userToken = user?.token;
