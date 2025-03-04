91ff40e90a3897d067b4af09d218b0dac95693d9
Joshua Daugherty <Joshua_Daugherty@insideranken.org>
2025-03-04 12:21:32 -0600

    Update page.tsx

--- bookin-app/app/(back)/Dashboard/settings/page.tsx
+++ bookin-app/app/(back)/Dashboard/settings/page.tsx
@@ -3,9 +3,7 @@ import Settings from "@/components/Dashboard/Settings/Settings"
 
 
 
-export const iframeHeight = "780px"
 
-export const containerClassName = "w-full h-full"
 
 export default function Page() {
   return (
